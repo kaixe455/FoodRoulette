7a6e048c8b73edebf862420a21f2e57a94946335
kaixe455 <48770955+kaixe455@users.noreply.github.com>
2024-01-06 10:39:05 +0100

    Adding variables

--- src/env.d.ts
+++ src/env.d.ts
@@ -3,6 +3,8 @@
 interface ImportMetaEnv {
 	readonly XATA_API_KEY: string;
 	readonly XATA_BRANCH?: string;
+	readonly REACT_API_URL: string;
+	readonly API_URL?: string;
 }
 
 interface ImportMeta {

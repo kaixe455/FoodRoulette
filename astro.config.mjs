91d39443b8cdfa62a1dce5e6db17120fd7204175
kaixe455 <48770955+kaixe455@users.noreply.github.com>
2024-01-07 16:42:40 +0100

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import node from "@astrojs/node";
-
+import vercelServerless from '@astrojs/vercel/serverless';
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
   output: "server",
-  adapter: node({
-    mode: "standalone"
-  })
+  adapter: vercelServerless()
 });
